53514028e64adea03e9c94df33160344f52aa6d2
d-pamneja <dpamneja@gmail.com>
2025-01-12 16:28:28 +0530

    Breakdown

---
+++ shared/header.php
@@ -0,0 +1,8 @@
+<header id="top" class="position-sticky top-0 start-0" style="z-index:10;">
+    <nav class="navbar bg-white fixed-top">
+        <div class="container-fluid">
+        <?php include 'header-top.php'; ?>
+        <?php include 'menu.php'; ?>
+        </div>
+    </nav>
+</header>
