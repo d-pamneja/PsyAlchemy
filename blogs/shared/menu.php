742bb865e7892b971a46c649670e1e31150c99a6
d-pamneja <dpamneja@gmail.com>
2025-01-12 18:58:16 +0530

    Edits

--- blogs/shared/menu.php
+++ blogs/shared/menu.php
@@ -15,36 +15,7 @@
     <div class="offcanvas-body">
     <ul class="navbar-nav flex-grow-1 p-4">
         <li class="nav-item">
-        <a class="nav-link active text-uppercase ls-4 text-white" aria-current="page" href="index.html">Home</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link text-uppercase ls-4 text-white" aria-current="page" href="index.html">Portfolio 
-        </a>
-        </li>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link text-uppercase ls-4 text-white" aria-current="page" href="index.html">Single Portfolio 
-        </a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link text-uppercase ls-4 text-white" aria-current="page" href="index.html">Single Post 
-        </a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link text-uppercase ls-4 text-white" aria-current="page" href="index.html">Styles 
-        </a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link text-uppercase ls-4 text-white" aria-current="page" href="index.html">Blog 
-        </a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link text-uppercase ls-4 text-white" aria-current="page" href="index.html">Team 
-        </a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link text-uppercase ls-4 text-white" aria-current="page" href="index.html">Contact 
-        </a>
+            <a class="nav-link active text-uppercase ls-4 text-white" aria-current="page" href="../index.php">Home</a>
         </li>
     </ul>
     </div>
---
+++ utils/services.php
@@ -0,0 +1,91 @@
+<section class="portfolio py-5">
+    <div class="container">
+      <div class="justify-content-center">
+          <div style="margin: 50px;">
+            <?php include 'home/qualifications.php'; ?>
+          </div>
+
+          <div id="filters" class="button-group d-flex flex-wrap gap-3 justify-content-center py-5" data-aos="fade-up">
+            <a href="#" class="btn btn-primary text-decoration-none text-uppercase is-checked"
+              data-filter=".photography">Photography</a>
+            <a href="#" class="btn btn-primary text-decoration-none text-uppercase" data-filter=".graphicdesign">Graphic
+              Design</a>
+            <a href="#" class="btn btn-primary text-decoration-none text-uppercase"
+              data-filter=".illustrations">Illustrations</a>
+            <a href="#" class="btn btn-primary text-decoration-none text-uppercase" data-filter=".branding">Branding</a>
+          </div>
+        </div>
+
+        <div class="grid p-0 clearfix row row-cols-2 row-cols-lg-3 row-cols-xl-4" data-aos="fade-up">
+          <div class="col mb-4 portfolio-item photography">
+            <a href="images/illustration-1.jpg" data-lightbox="illustration-1" data-title="illustration-1"
+              title="Sample Caption goes here for Portfolio Item 1."><img src="images/illustration-1.jpg"
+                class="img-fluid rounded-4" alt="portfolio"></a>
+          </div>
+          <div class="col mb-4 portfolio-item photography graphicdesign illustrations">
+            <a href="images/illustration-2.jpg" data-lightbox="illustration-2" data-title="illustration-2"
+              title="Sample Caption goes here for Portfolio Item 2."><img src="images/illustration-2.jpg"
+                class="img-fluid rounded-4" alt="portfolio"></a>
+          </div>
+          <div class="col mb-4 portfolio-item branding graphicdesign">
+            <a href="images/illustration-3.jpg" data-lightbox="illustration-3" data-title="illustration-3"
+              title="Sample Caption goes here for Portfolio Item 3."><img src="images/illustration-3.jpg"
+                class="img-fluid rounded-4" alt="portfolio"></a>
+          </div>
+          <div class="col mb-4 portfolio-item graphicdesign">
+            <a href="images/illustration-4.jpg" data-lightbox="illustration-4" data-title="illustration-4"
+              title="Sample Caption goes here for Portfolio Item 4."><img src="images/illustration-4.jpg"
+                class="img-fluid rounded-4" alt="portfolio"></a>
+          </div>
+          <div class="col mb-4 portfolio-item photography illustrations">
+            <a href="images/illustration-5.jpg" data-lightbox="illustration-5" data-title="illustration-5"
+              title="Sample Caption goes here for Portfolio Item 5."><img src="images/illustration-5.jpg"
+                class="img-fluid rounded-4" alt="portfolio"></a>
+          </div>
+          <div class="col mb-4 portfolio-item graphicdesign">
+            <a href="images/illustration-1.jpg" data-lightbox="illustration-1" data-title="illustration-1"
+              title="Sample Caption goes here for Portfolio Item 6."><img src="images/illustration-1.jpg"
+                class="img-fluid rounded-4" alt="portfolio"></a>
+          </div>
+          <div class="col mb-4 portfolio-item graphicdesign photography">
+            <a href="images/illustration-2.jpg" data-lightbox="illustration-2" data-title="illustration-2"
+              title="Sample Caption goes here for Portfolio Item 7."><img src="images/illustration-2.jpg"
+                class="img-fluid rounded-4" alt="portfolio"></a>
+          </div>
+          <div class="col mb-4 portfolio-item illustrations graphicdesign">
+            <a href="images/illustration-3.jpg" data-lightbox="illustration-3" data-title="illustration-3"
+              title="Sample Caption goes here for Portfolio Item 8."><img src="images/illustration-3.jpg"
+                class="img-fluid rounded-4" alt="portfolio"></a>
+          </div>
+          <div class="col mb-4 portfolio-item branding photography graphicdesign">
+            <a href="images/illustration-4.jpg" data-lightbox="illustration-4" data-title="illustration-4"
+              title="Sample Caption goes here for Portfolio Item 9."><img src="images/illustration-4.jpg"
+                class="img-fluid rounded-4" alt="portfolio"></a>
+          </div>
+          <div class="col mb-4 portfolio-item illustrations graphicdesign branding">
+            <a href="images/illustration-5.jpg" data-lightbox="illustration-5" data-title="illustration-5"
+              title="Sample Caption goes here for Portfolio Item1 0."><img src="images/illustration-5.jpg"
+                class="img-fluid rounded-4" alt="portfolio"></a>
+          </div>
+          <div class="col mb-4 portfolio-item photography">
+            <a href="images/illustration-1.jpg" data-lightbox="illustration-1" data-title="illustration-1"
+              title="Sample Caption goes here for Portfolio Item1 1."><img src="images/illustration-1.jpg"
+                class="img-fluid rounded-4" alt="portfolio"></a>
+          </div>
+          <div class="col mb-4 portfolio-item photography">
+            <a href="images/illustration-2.jpg" data-lightbox="illustration-2" data-title="illustration-2"
+              title="Sample Caption goes here for Portfolio Item1 2."><img src="images/illustration-2.jpg"
+                class="img-fluid rounded-4" alt="portfolio"></a>
+          </div>
+
+        </div>
+
+        <div class="text-center p-3">
+          <a href="index.html" class="btn btn-outline-dark btn-lg mt-3 text-uppercase text-decoration-none">
+            View All Works
+          </a>
+        </div>
+
+        </div>
+    </div> 
+</section>
