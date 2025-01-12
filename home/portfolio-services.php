4774769e75a747c9a583596a26b003ea21b11718
d-pamneja <dpamneja@gmail.com>
2025-01-12 17:38:24 +0530

    Blog

--- home/portfolio-services.php
+++ home/portfolio-services.php
@@ -3,14 +3,28 @@
       <div class="justify-content-center">
 
         <div class="row justify-content-center">
-
-          <div class="col-lg-6" data-aos="fade-up">
+          <div class="col-lg-col-10" data-aos="fade-up">
             <div class="section-header text-center">
               <h4 class="fw-bold fs-2 txt-fx slide-up">
-                As a passionate life coach and entrepreneur, I'm dedicated to guiding you on your journey to success and
-                fulfillment.
+                Hello! I’m Palchhin Sharma, a passionate and dedicated mental health professional with a strong grounding in psychological theories and a drive to translate knowledge into impactful practice and research. With a special focus on Child Psychology and Research, I bring expertise in mental health diagnostics, including proficiency in various assessment tools and techniques.
               </h4>
             </div><!--section-header-->
+          </div>
+
+          <?php include 'home/references.php'; ?>
+
+          <div class="col-lg-col-10" data-aos="fade-up">
+            <div class="section-header text-center">
+              <h4 class="fw-bold fs-2 txt-fx slide-up">
+                I have a deep understanding of the physiological, emotional, and social factors shaping human behavior, coupled with a solid foundation in Clinical Psychology and Research Methodology. My journey is fueled by a genuine commitment to advancing mental health awareness and therapeutic approaches through innovation and empathy.
+                Let’s connect and explore the ever-evolving world of psychology together, turning insights into meaningful change!
+              </h4>
+            </div><!--section-header-->
+          </div>
+
+
+          <div style="margin: 50px;">
+            <?php include 'home/qualifications.php'; ?>
           </div>
 
           <div id="filters" class="button-group d-flex flex-wrap gap-3 justify-content-center py-5" data-aos="fade-up">
