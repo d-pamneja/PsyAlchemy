800c85e72c3448e2bee36404256ccbeb6c6d9e1a
d-pamneja <dpamneja@gmail.com>
2025-01-12 23:15:49 +0530

    Edits

--- shared/header.php
+++ shared/header.php
@@ -1,7 +1,13 @@
 <header id="top" class="position-sticky top-0 start-0" style="z-index:10;">
     <nav class="navbar bg-white fixed-top">
         <div class="container-fluid">
-        <?php include 'header-top.php'; ?>
+        
+        <div class="d-flex align-items-center g-4">
+            <a class="navbar-brand d-flex" href="index.php">
+                <img src="images/name-logo.png" class="img-fluid" id="logo" height="200px" width="200px">
+            </a>
+        </div>
+        
         <?php include 'menu.php'; ?>
         </div>
     </nav>
